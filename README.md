c0d661a2dcf018cf92bddd4e98ea043919b88b06
Rizky Roma Dhony <romadhonyrizky982@gmail.com>
2021-10-25 16:48:03 +0700

    menambahkan file

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # latihan
+## belajarpython
+### membuat biodata
+![gambar1](ss/ss1.png)
---
+++ belajarpython.py
@@ -0,0 +1,8 @@
+nama=input("siapa nama kamu:")
+namapanggilan=input("biasa dipanggil apa:")
+nim=input("nim kamu berapa:")
+ttl=input("dimana,kapan anda lahir:")
+telepon=input("berapa no hp kamu:")
+alamat=input("dimana kamu tinggal:")
+
+print("jadi nama kamu adalah",nama,"biasa dipanggil",namapanggilan,"nim kamu adalah",nim,"kamu lahir di",ttl,"no kamu adalah",telepon,"rumah kamu di",alamat)
